32548eababe1d847559dd93358d7250f2c18c698
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-11-12 20:47:17 -0500

    task

--- API/prediction_function.py
+++ API/prediction_function.py
@@ -1,30 +1,33 @@
+###############################################
+# TASK
+# --------------------
+#
+# 1. import JPG file into python
+# 2. pass the JPG into function testimage as an argument(final line)
+# 3. Add model from kaggle to testimage function so that it returns a prediction
+# 4. If model is working it shall return a number that is greater than 0
+#
+###############################################
 import base64
 
 image = ""
 
-def init_model():
-    pass
-    
 
-
-init_model()
-
-
-def testimage(base64image:str = None) -> int:
+def testimage(image = None) -> int:
     """Test image and returns confidence level
 
     Args:
         base64image (str, optional): JPG image of mole. Defaults to None.
 
     Returns:
-        int: percent chance
+        int: percent chance of having melanoma as an int out of 100
     """
-    image = base64.decode()
-    test_result = 0
+    test_result:int = 0
+    
     
     
 
-    return test_result
+    return int(test_result)
 
 # test function
-print(testimage(image))
+print(f" This image has a {testimage(image)}% chance of being melanoma")
