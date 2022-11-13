0472fa8150d7cff1f82f01259826b0ad557bbf6e
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-11-12 19:22:16 -0500

    function to predict image

---
+++ API/prediction_function.py
@@ -0,0 +1,14 @@
+import base64
+
+image = ""
+
+
+def testimage(base64image:str = None) -> int:
+    image = base64.decode()
+    test_result = 0
+    
+
+    return test_result
+
+
+print(testimage(base64image= image))
